52ed83286feeb92de5a7fc7b62b3ec3a0ab7139f
Erica Rottman <erica7650@gmail.com>
2017-06-27 08:37:11 -0700

    units seeds

--- db/seeds/4_units.js
+++ db/seeds/4_units.js
@@ -1,13 +1,26 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = (knex) => {
+  return knex('units').del()
+  .then(() => {
+    return knex('units').insert([{
+      id:1,
+      tenent_id: 1,
+      facility_id: 2,
+      unit_number: 111
+      }, {
+      id:2,
+      tenent_id: 2,
+      facility_id: 2,
+      unit_number: 222
+      },{
+      id:3,
+      tenent_id: 3,
+      facility_id: 3,
+      unit_number: 333
+    }]);
+  })
+  .then(() => {
+    return knex.raw(
+      "SELECT setval('units_id_seq', (SELECT MAX(id) FROM units));"
+    );
+  });
 };
